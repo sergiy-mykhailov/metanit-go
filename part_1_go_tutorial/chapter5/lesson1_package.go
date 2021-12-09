41d94a6d56a1b6b899d52d45b301b4182b7f5b28
serhii_mykhailov <serhii_mykhailov@external.mckinsey.com>
2021-12-09 19:23:27 +0200

    refactored folder structure

--- part_1_go_tutorial/chapter5/lesson1_package.go
+++ part_1_go_tutorial/chapter5/lesson1_package.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
-	"metanit_go/chapter5/computation"
+	computation2 "metanit_go/part_1_go_tutorial/chapter5/computation"
 )
 
 // package
@@ -13,5 +13,5 @@ func main() {
 
 	fmt.Println("Sqrt", math.Sqrt(16))
 	fmt.Println("factorial", factorial(5))
-	fmt.Println("computation.factorial", computation.Factorial(6))
+	fmt.Println("computation.factorial", computation2.Factorial(6))
 }
